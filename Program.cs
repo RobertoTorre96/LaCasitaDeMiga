bcc8d5b7b8448fae3bd3501e2fa0fc3a8834043d
Roberto Torre <torreroberto1996@gmail.com>
2026-06-19 21:59:00 -0300

    programCs

--- Program.cs
+++ Program.cs
@@ -8,41 +8,49 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// --- CONFIGURACIÓN DE BASE DE DATOS (NUEVA LOGICA NEON) ---
 var connectionString = builder.Configuration.GetConnectionString("PostgresConnection");
 
-// Add services to the container.
+// Declaramos y leemos la URL nativa de Neon desde las variables de entorno de Render
+var envUrl = Environment.GetEnvironmentVariable("NEON_DATABASE_URL");
+if (!string.IsNullOrEmpty(envUrl))
+{
+    connectionString = envUrl;
+}
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(connectionString));
+// ---------------------------------------------------------------
 
+// Add services to the container.
 builder.Services.AddAutoMapper(typeof(Program));
 builder.Services.AddScoped<IBrandService, BrandServiceImpl>();
 builder.Services.AddScoped<ICategoryService, CategoryServiceImpl>();
-builder.Services.AddScoped<IOrderService, OrderServiceImpl>(); ;
+builder.Services.AddScoped<IOrderService, OrderServiceImpl>();
 builder.Services.AddScoped<IProductService, ProductServiceImpl>();
 
-
-//exceptions
+// Exceptions
 builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
 builder.Services.AddProblemDetails();
-
-
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
 app.UseExceptionHandler();
+
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment()) {
+if (app.Environment.IsDevelopment())
+{
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
