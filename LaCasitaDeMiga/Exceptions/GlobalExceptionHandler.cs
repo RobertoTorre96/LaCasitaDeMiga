5eba36ccd56fb4d9335e013612515c66e5a984f6
Roberto Torre <torreroberto1996@gmail.com>
2026-07-23 22:03:54 -0300

    add log

--- LaCasitaDeMiga/Exceptions/GlobalExceptionHandler.cs
+++ LaCasitaDeMiga/Exceptions/GlobalExceptionHandler.cs
@@ -19,9 +19,10 @@ namespace LaCasitaDeMiga.Exceptions {
                 AlreadyExistsException => (StatusCodes.Status409Conflict, "Conflicto: El recurso ya existe"),
                 ValidationException => (StatusCodes.Status400BadRequest, "Error de validación"),
                 BadRequestException => (StatusCodes.Status400BadRequest, "Solicitud incorrecta"),
+                ConflictException => (StatusCodes.Status409Conflict, "Conflicto de concurrencia"),
+                LaCasitaDeMiga.Exceptions.UnauthorizedException => (StatusCodes.Status401Unauthorized, "No autorizado"),
                 UnauthorizedAccessException => (StatusCodes.Status401Unauthorized, "No autorizado"),
 
-                // Cualquier otro error no controlado (como fallos de Postgres/Docker) cae aquí
                 _ => (StatusCodes.Status500InternalServerError, "Error interno del servidor")
             };
 
